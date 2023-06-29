31c1830950e1ff9715d280bd4cd2ba1fd9b20cb6
y00tss <shepel9119@gmail.com>
2023-06-29 21:37:30 +0300

    iter, next

---
+++ __methods__/iter, next.py
@@ -0,0 +1,19 @@
+class FRange:
+    def __init__(self, start=0.0, stop=0.0, step=1.0):
+        self.start = start
+        self.stop = stop
+        self.step = step
+
+    def __iter__(self):
+        self.value = self.start - self.step
+        return self
+    def __next__(self):
+        if self.value + self.step < self.stop:
+            self.value += self.step
+            return self.value
+        else:
+            raise StopIteration
+
+fr = FRange(1, 5, 0.5)
+for x in fr:
+    print(x)
